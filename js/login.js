356573ef7390ed6d744dffc1057fe13cd577f511
Biahacks <azubuike.me@gmail.com>
2021-03-23 18:33:11 +0000

    initial upload

--- js/login.js
+++ js/login.js
@@ -3,7 +3,7 @@ function login() {
     var pass=document.getElementById('password').value;
 
     if (uname=="admin" && pass=="0000") {
-        location.assign("http://localhost/shop/index.html");
+        window.open("http://localhost/shop/index.html");
     }else {
         window.alert("login failed, try again!");
     }
